8d1e6e0e53a4d06a8f941d51af99dde3a2d2687b
Anderson <andersondiasmd25@gmail.com>
2025-08-20 09:20:43 -0300

    Adiciona funcionalidades ao módulo de usuário, incluindo métodos para obter e criar usuários, e integra o PrismaModule. Atualiza dependências no package.json e package-lock.json.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Body, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+
+    @Get()
+    async getUsers() {
+        return this.userService.getUsers();
+    }
+    
+    @Post()
+    async createUser(@Body() user: CreateUserDto) {
+        return this.userService.createUser(user);
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async getUsers() {
+        return this.prisma.user.findMany();
+    }
+
+    async createUser(user: CreateUserDto) {
+        return this.prisma.user.create({
+            data: user,
+        });
+    }
+}
