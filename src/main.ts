4c58311eb6e3ee80c370eeb8136d2a9447ed0fee
Anderson <andersondiasmd25@gmail.com>
2025-08-20 09:29:17 -0300

    Adiciona configuração de validação global no bootstrap da aplicação, utilizando ValidationPipe para gerenciar propriedades e tipos nas requisições.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Configuração de validação global
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Remove propriedades não decoradas
+      forbidNonWhitelisted: true, // Rejeita requisições com propriedades não permitidas
+      transform: true, // Transforma automaticamente tipos
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
