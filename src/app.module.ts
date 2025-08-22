f0e2c9a271cbd789681bcf4ac4e8d31b85d9a60c
Anderson <andersondiasmd25@gmail.com>
2025-08-22 08:34:54 -0300

    Adiciona o módulo @nestjs/throttler ao projeto e configura limites de requisições no AppModule para controle de taxa.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,18 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [UserModule, PrismaModule, AuthModule],
+  imports: [
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 10,
+    }]),
+    UserModule, 
+    PrismaModule, 
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
