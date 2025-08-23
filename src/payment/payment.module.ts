8e2987a1507cb185daa7cf8480390c2282dc8bf6
anderson <andersondiasmd25@gmail.com>
2025-08-23 16:29:50 -0300

    Adiciona o PaymentController ao PaymentModule e implementa um corpo vazio para o PaymentService, preparando a estrutura para futuras implementações.

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
+import { PaymentController } from './payment.controller';
 import { ConfigModule } from '@nestjs/config';
 import stripeConfig from 'src/config/stripe.config';
 
 @Module({
   imports: [ConfigModule.forFeature(stripeConfig)],
   providers: [PaymentService],
+  controllers: [PaymentController],
   exports: [PaymentService]
 })
 export class PaymentModule {}
